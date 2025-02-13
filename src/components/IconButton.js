54d5ede1922819768e86b72d0c3f4d447b423372
thuongpv <thuongpv@sunhouse.com.vn>
2025-02-13 13:55:32 +0700

    Initial commit

---
+++ src/components/IconButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+const IconButton = ({ title = "Icon", onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <FontAwesome name="camera" size={20} color="black" style={styles.icon} />
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#EDE7F6',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  text: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
+
+export default IconButton;
